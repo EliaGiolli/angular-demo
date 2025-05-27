eeca30600f7af1efbdc93cae68956f22a480b4c8
Elia Giolli <you@example.com>
2025-05-27 14:26:24 +0200

    removed angular material

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserProps } from './user-card.types';
 
 @Component({
   selector: 'app-user-card',
   imports: [],
   standalone: true,
   templateUrl: './user-card.component.html',
-  styleUrl: './user-card.component.css'
+  styleUrl: './user-card.component.scss'
 })
 export class UserCardComponent {
-
+  //the ! tells typescript that the props will arrive from outside this file
+  @Input() user!: UserProps;
 }
---
+++ src/app/user-card/user-card.types.ts
@@ -0,0 +1,10 @@
+export enum UserRole{
+    User = 'user',
+    Admin = 'admin'
+}
+
+export interface UserProps {
+    userName: string,
+    email: string,
+    role: UserRole
+}
