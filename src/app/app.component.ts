3f03f7b19335b4115c41cd26a9d76b49d6994f73
Elia Giolli <you@example.com>
2025-05-27 12:04:16 +0200

    restyled the main app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from './user-card/user-card.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-card',
   imports: [],
+  standalone: true,
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css'
 })
